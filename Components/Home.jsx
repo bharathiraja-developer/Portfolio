ab68c19d0a87741c349b3ca288843b0208882534
bharathiraja-developer <bharathiraja798@gmail.com>
2024-05-06 11:21:25 +0530

    animation added

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -1,60 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../Styles/Home.css";
 import { motion } from "framer-motion";
 import home from "../image.svg";
 import resume from "../Bharathiraja B.pdf";
+import { TypeAnimation } from "react-type-animation";
 
 function Home() {
-  const [type, setType] = useState(" ");
-  const [blink, setBlink] = useState(true);
-  useEffect(typing, []);
-  let datas = [
-    "Frontend Developer",
-    "React JS Developer",
-    "Node JS Developer",
-    "MERN Stack Developer",
-  ];
-  let i = 0;
-  let j = 0;
-  let word = "";
-  function typing() {
-    if (j == 4) {
-      j = 0;
-    }
-    if (i < datas[j].length && blink) {
-      word += datas[j].charAt(i);
-      setType(word);
-      i++;
-      if (i == datas[j].length) {
-        rTyping();
-        return null;
-      }
-      setTimeout(typing, 200);
-    }
-  }
-
-  function rTyping() {
-    if (i != 0) {
-      let nWord = word.substring(0, i);
-      setType(nWord);
-      i--;
-      setTimeout(rTyping, 200);
-    } else {
-      j++;
-      word = "";
-      i = 0;
-      typing();
-    }
-  }
-
   return (
     <div id="home" className="container-fluid mt-5">
       <div className="container">
         <div className="row">
           <div className="col mt-4 mt-lg-5">
             <h1 className="bigSize text-dark">Hi, I am Bharathiraja</h1>
+
             <h1 className="box">
-              <div className="typingtext bigSize text-primary">{type}</div>
+              <TypeAnimation
+                className="typingtext bigSize text-primary"
+                sequence={[
+                  "Frontend Developer",
+                  1000,
+                  "React JS Developer",
+                  1000,
+                  "Node JS Developer",
+                  1000,
+                  "MERN Stack Developer",
+                  1000,
+                ]}
+                speed={50}
+                repeat={Infinity}
+              />
             </h1>
             <div className="col-12">
               <p className="fs-4 fw-normal w-75 mt-3">
